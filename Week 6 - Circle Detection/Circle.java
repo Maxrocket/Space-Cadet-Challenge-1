6a35979e939e16810412a0813a985e8d2a093959
Maxrocket <30619724+Maxrocket@users.noreply.github.com>
2018-11-16 01:29:59 +0000

    Add files via upload

---
+++ Week 6 - Circle Detection/Circle.java
@@ -0,0 +1,13 @@
+package circldetection.gfx;
+
+public class Circle {
+    
+    public double x, y, r;
+
+    public Circle(double x, double y, double r) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+    }
+    
+}
